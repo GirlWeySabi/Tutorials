7465db6ab1247606f8ad37ce41bfb05abd53220e
1mustyz <onemusty.z@gmail.com>
2020-11-11 11:45:53 +0100

    reaction table mustapha

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const db =require('./model')
 
 
 var app = express();
+const reactionRoute = require('./route/reaction.route');
 
-db.sequelize.sync({force : true});
+db.sequelize.sync();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/reaction', reactionRoute);
+
 
 
 // catch 404 and forward to error handler
